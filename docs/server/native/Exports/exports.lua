f0593934504b63f410737d0b6c3e6b0e42f7a136
Kakarot <joshuaeger@hypersoniclabs.co>
2025-12-05 14:37:43 -0600

    ?

--- docs/server/native/Exports/exports.lua
+++ docs/server/native/Exports/exports.lua
@@ -8,10 +8,10 @@ local Exports = {}
 ---@usage exports("mypackage", "myFunction", function(arg1, arg2) return result end)
 function exports(resourceName, exportName, fn) end
 
----Calls an export function from another package
+--- Calls an export function from another package
 ---@param resourceName string The package name containing the export
 ---@param exportName string The name of the export function to call
----@param ... any Arguments to pass to the export function (supports UObject references and nested tables)
----@return any Returns the values returned by the export function
----@usage local result = exports["otherpackage"]:myFunction(arg1, arg2)
-function exports[resourceName]:exportName(...) end
+---@param ... any Arguments passed to the export function (supports UObject and nested tables)
+---@return any Return values from the export function
+---@usage exports["otherpackage"]:myFunction(arg1, arg2)
+function exports.Invoke(resourceName, exportName, ...) end
